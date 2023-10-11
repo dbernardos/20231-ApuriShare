e21f85a9ec87c66609aacad581f1f7f7717a2b25
Priscila de Mira <100444956+PrisciladeMira@users.noreply.github.com>
2023-10-11 14:17:10 -0300

    Update apurishare.sql

--- ApuriShare-src/sql/apurishare.sql
+++ ApuriShare-src/sql/apurishare.sql
@@ -1,11 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 28-Set-2023 às 21:23
--- Versão do servidor: 10.4.21-MariaDB
--- versão do PHP: 7.4.23
+CREATE DATABASE apurishare;
+
+USE apurishare;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -17,21 +12,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Banco de dados: `apurishare`
---
-
-CREATE DATABASE apurishare;
-
--- --------------------------------------------------------
-
--- --------------------------------------------------------
-
-USE apurishare;
-
---
--- Estrutura da tabela `atividade`
---
 
 CREATE TABLE `atividade` (
   `codigo` int NOT NULL,
@@ -41,11 +21,6 @@ CREATE TABLE `atividade` (
   `fk_usuario` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `sala`
---
 
 CREATE TABLE `sala` (
   `chaveAcesso` int(6) NOT NULL,
@@ -57,9 +32,6 @@ CREATE TABLE `sala` (
   `fk_situacao` int(6) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Extraindo dados da tabela `sala`
---
 
 INSERT INTO `sala` (`chaveAcesso`, `atividade`, `observacao`, `arquivo`, `nome`, `qntUsers`, `fk_situacao`) VALUES
 (46, 'Onde se localiza Machu Piccho?', 'Peru', NULL, 'Geografia', 4, 5),
@@ -68,11 +40,6 @@ INSERT INTO `sala` (`chaveAcesso`, `atividade`, `observacao`, `arquivo`, `nome`,
 (49, 'A que temperatura a Ã¡gua ferve?', '100Â°', NULL, 'FÃ­sica', 6, 5),
 (50, 'Qual o maior planeta do sistema solar?', 'JÃºpiter', NULL, 'Astronomia', 5, 3);
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `sala_usuario`
---
 
 CREATE TABLE `sala_usuario` (
   `id_sala_usuario` int(6) NOT NULL,
@@ -81,11 +48,6 @@ CREATE TABLE `sala_usuario` (
   `tipoUsuario` enum('criador','participante') NOT NULL DEFAULT 'participante'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `situacao`
---
 
 CREATE TABLE `situacao` (
   `idSituacao` int(6) NOT NULL,
@@ -93,9 +55,6 @@ CREATE TABLE `situacao` (
   `descricaoSituacao` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Extraindo dados da tabela `situacao`
---
 
 INSERT INTO `situacao` (`idSituacao`, `statusSituacao`, `descricaoSituacao`) VALUES
 (1, 'Sala Criada', 'Iniciar Atividade Individual'),
@@ -104,11 +63,6 @@ INSERT INTO `situacao` (`idSituacao`, `statusSituacao`, `descricaoSituacao`) VAL
 (4, 'Compartilhar Atividades', 'Finalizar Atividade'),
 (5, 'Finalizada', '');
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `usuario`
---
 
 CREATE TABLE `usuario` (
   `nickname` varchar(100) NOT NULL,
@@ -116,9 +70,6 @@ CREATE TABLE `usuario` (
   `senha` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Extraindo dados da tabela `usuario`
---
 
 INSERT INTO `usuario` (`nickname`, `nome`, `senha`) VALUES
 ('belly.90', 'Isabelly Santos', '1323'),
@@ -127,82 +78,48 @@ INSERT INTO `usuario` (`nickname`, `nome`, `senha`) VALUES
 ('portuga.2004', 'Rafael Portugal', '2004'),
 ('raque.lis', 'Raquel da Silva', '1020');
 
---
--- Índices para tabelas despejadas
---
 
---
--- Índices para tabela `atividade`
---
 ALTER TABLE `atividade`
   ADD PRIMARY KEY (`codigo`);
 
---
--- Índices para tabela `sala`
---
+
 ALTER TABLE `sala`
   ADD PRIMARY KEY (`chaveAcesso`);
 
---
--- Índices para tabela `sala_usuario`
---
+
 ALTER TABLE `sala_usuario`
   ADD PRIMARY KEY (`id_sala_usuario`);
 
---
--- Índices para tabela `situacao`
---
+
 ALTER TABLE `situacao`
   ADD PRIMARY KEY (`idSituacao`);
 
---
--- Índices para tabela `usuario`
---
+
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`nickname`);
 
---
--- AUTO_INCREMENT de tabelas despejadas
---
 
---
--- AUTO_INCREMENT de tabela `atividade`
---
 ALTER TABLE `atividade`
   MODIFY `codigo` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
---
--- AUTO_INCREMENT de tabela `sala`
---
+
 ALTER TABLE `sala`
   MODIFY `chaveAcesso` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
---
--- AUTO_INCREMENT de tabela `sala_usuario`
---
+
 ALTER TABLE `sala_usuario`
   MODIFY `id_sala_usuario` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
 
---
--- Restrições para despejos de tabelas
---
 
---
--- Limitadores para a tabela `atividade`
---
 ALTER TABLE `atividade`
   ADD CONSTRAINT `fk_usuario` FOREIGN KEY (`fk_usuario`) REFERENCES `usuario` (`nickname`),
   ADD CONSTRAINT `fk_sala` FOREIGN KEY (`fk_sala`) REFERENCES `sala` (`chaveAcesso`);
 
---
--- Limitadores para a tabela `sala`
---
+
 ALTER TABLE `sala`
   ADD CONSTRAINT `const_sala_situacao` FOREIGN KEY (`fk_situacao`) REFERENCES `situacao` (`idSituacao`);
 
---
--- Limitadores para a tabela `sala_usuario`
---
+
 ALTER TABLE `sala_usuario`
   ADD CONSTRAINT `sala_usuario_ibfk_1` FOREIGN KEY (`fk_sala`) REFERENCES `sala` (`chaveAcesso`),
   ADD CONSTRAINT `sala_usuario_ibfk_2` FOREIGN KEY (`fk_usuario`) REFERENCES `usuario` (`nickname`);

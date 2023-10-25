2c145cd80197137b670467c9688400d74d2191f2
Priscila de Mira <100444956+PrisciladeMira@users.noreply.github.com>
2023-10-25 16:39:07 -0300

    começando modificações na tela inicial

--- ApuriShare-src/telas/menu.php
+++
@@ -1,7 +0,0 @@
-<div class="cabecalho">
-        <h2><a href='tela_inicial.php'>ApuriShare</a></h2>
-        <?php
-            echo "<h4><a href='iniciacao_partida.php'>Salas de {$_SESSION['nickname']}</a></h4>";
-        ?>
-</div>
-<br><br>
--- ApuriShare-src/telas/tela_inicial.php
+++ ApuriShare-src/telas/tela_inicial.php
@@ -3,34 +3,40 @@
 <!DOCTYPE html>
 <html>
 
-<head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <link rel="stylesheet" href="./css/geral.css">
-    <link rel="stylesheet" href="./css/tela_inicial.css">
-    <meta charset="UTF-8">
-    <title>ApuriShare</title>
-</head>
-<body>
-<?php include('menu.php');?>
-    <center>
-        <div class="centro">
-            <h1>Entre ou Crie Salas</h1><br><br>
-            <div class="botoes">
-                <a href="./entrar_sala.php"><button type="button" class="btn btn-outline-dark"> Entrar </button></a>
-                <a href="./criar_sala.php"><button type="button" class="btn btn-outline-dark"> Criar </button></a>    
+    <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>ApuriShare</title>
+    </head>
+    <body>
+        
+        
+        
+        <?php
+            echo "<h4><a href='iniciacao_partida.php'>Salas de {$_SESSION['nickname']}</a></h4>";
+        ?>
+        <center>
+            <div class="centro">
+                <h1>Entre ou Crie Salas</h1><br><br>
+                <div class="botoes">
+                    <a href="./entrar_sala.php"><button type="button" class="btn btn-outline-dark"> Entrar </button></a>
+                    <a href="./criar_sala.php"><button type="button" class="btn btn-outline-dark"> Criar </button></a>    
+                </div>
+                <br>
+                <p>Entre em salas para realizar atividades e desafios feitos pelos usúario!</p>
+                <a href="" class="link-opacity-10-hover">Não sabe como funciona? Clique aqui</a>
+                <br><br>
             </div>
-            <br>
-            <p>Entre em salas para realizar atividades e desafios feitos pelos usúario!</p>
-            <a href="" class="link-opacity-10-hover">Não sabe como funciona? Clique aqui</a>
+        </center>
             <br><br>
-        </div>
-    </center>
-    <br><br>
-    <div class="sair">
-        <a href="./sair.php"><button type="submit" class="btn btn-outline-dark"><img src="./img/Icon_Sair.png" width="20" height="20"><strong> Sair da Conta</strong></button></a>
-    </div>
-    
+            <div class="sair">
+                <a href="./sair.php"><button type="submit" class="btn btn-outline-dark"><img src="./img/Icon_Sair.png" width="20" height="20"><strong> Sair da Conta</strong></button></a>
+            </div>
+    </body>   
 
 </html>

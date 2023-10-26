e3c9841dc7e3f598695244699fd8593290df1009
Priscila de Mira <100444956+PrisciladeMira@users.noreply.github.com>
2023-10-26 14:12:24 -0300

    .

--- ApuriShare-src/telas/tela_inicial.php
+++ ApuriShare-src/telas/tela_inicial.php
@@ -15,10 +15,23 @@
     </head>
     <body>
         
-        
+        <!-- Cabeçalho -->
+    <nav id="header" class="navbar navbar-expand-lg navbar-light bg-light fixed-top" style="border-bottom: 1px solid #ccc;">
+        <a class="navbar-brand" href="tela_inicial.php">
+            <img src="./img/logo_preta.png" alt="Logo do ApuriShare" style="max-height: 50px;">
+        </a>
+
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item" style="">
+                    <?php
+                        echo "<h4><a class='nav-link text-dark' href='iniciacao_partida.php'>Salas de {$_SESSION['nickname']}</a></h4>";
+                    ?>
+                
+
+    </nav>
         
         <?php
-            echo "<h4><a href='iniciacao_partida.php'>Salas de {$_SESSION['nickname']}</a></h4>";
+            echo "<h4><a href='iniciacao_partida.php'>{$_SESSION['nickname']}</a></h4>";
         ?>
         <center>
             <div class="centro">
@@ -37,6 +50,14 @@
             <div class="sair">
                 <a href="./sair.php"><button type="submit" class="btn btn-outline-dark"><img src="./img/Icon_Sair.png" width="20" height="20"><strong> Sair da Conta</strong></button></a>
             </div>
+
+            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+                integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+                crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"
+                integrity="sha384-pzjw8f+ua7Kw1TIq0v8FqFjcJ6pajs/rfdfs3SO+kAx2jc5Pv5B1f5F6F5F5F5F5F5"
+                crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.min.js"></script>
     </body>   
 
 </html>

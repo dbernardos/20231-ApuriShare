d14cfb15402c0942744ad20e5cf67fca90889324
Priscila de Mira <100444956+PrisciladeMira@users.noreply.github.com>
2023-11-05 17:18:45 -0300

    .

--- ApuriShare-src/telas/criar_sala.php
+++ ApuriShare-src/telas/criar_sala.php
@@ -32,7 +32,7 @@ if(isset($_POST['btnCriarSala'])){
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <title>ApuriShare - Criar Sala</title>
 <style>
     body {
